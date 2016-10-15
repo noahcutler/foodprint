1e431d892fadf9f4812b18985e68179122a56a9f
Russell Gens <wuddlej@gmail.com>
2016-10-14 23:29:02 -0400

    made most basic server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-this is the file
+//Express initialization
+var express = require('express');
+var app = express();
+
+app.get('/', function (request, response) {
+	response.sendFile('login.html', {
+		root: __dirname
+	});
+});
+
+app.listen(process.env.PORT || 3000);
