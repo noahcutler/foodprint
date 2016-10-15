b876efde7a3f4c873aa6a133b10b37e71dbfa150
Russell Gens <wuddlej@gmail.com>
2016-10-15 10:26:25 -0400

    framework for posting works

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); // Required if we need to use HTTP query or post parameters
+
 var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/local_data;"
 var MongoClient = require('mongodb').MongoClient, format = require('util').format;
 var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
 	db = databaseConnection;
 });
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname));
 
@@ -29,7 +34,10 @@ app.post('/inputFood', function (request, response) {
 	response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "X-Requested-With");
 
-    console.log(request);
+    console.log(request.body);
+    response.sendFile('input.html', {
+    	root: __dirname
+    });
 
 //    var foods{};
 //
