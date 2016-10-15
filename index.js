97fbefd274a9c420392ba6c9a4d180021f870eed
Russell Gens <wuddlej@gmail.com>
2016-10-15 01:38:04 -0400

    added get for input.html

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 //Express initialization
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost/local_data;"
+var MongoClient = require('mongodb').MongoClient, format = require('util').format;
+var db = MongoClient.connect(mongoUri, function(error, databaseConnection) {
+	db = databaseConnection;
+});
 
 app.get('/', function (request, response) {
 	response.sendFile('login.html', {
 		root: __dirname
 	});
 });
+
+app.get('/input', function (request, response) {
+	response.sendFile('input.html', {
+		root: __dirname
+	});
+});
+
+app.post('/submit_meal', function (request, response) {
+
+})
+
 
 app.listen(process.env.PORT || 3000);
